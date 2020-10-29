d15d4c1a052760297829693032f501f46b881f2a
diego <masnimpu@gmail.com>
2020-10-29 15:28:38 +0300

    Blizzard api routing

---
+++ wow/router/character.py
@@ -0,0 +1,25 @@
+#  ██╗░░░░░██╗███╗░░██╗░██████╗░░░░██████╗░██╗░░░░░░█████╗░░█████╗░██╗░░██╗
+#  ██║░░░░░██║████╗░██║██╔════╝░░░░██╔══██╗██║░░░░░██╔══██╗██╔══██╗██║░██╔╝
+#  ██║░░░░░██║██╔██╗██║██║░░██╗░░░░██████╦╝██║░░░░░███████║██║░░╚═╝█████═╝░
+#  ██║░░░░░██║██║╚████║██║░░╚██╗░░░██╔══██╗██║░░░░░██╔══██║██║░░██╗██╔═██╗░
+#  ███████╗██║██║░╚███║╚██████╔╝░░░██████╦╝███████╗██║░░██║╚█████╔╝██║░╚██╗
+#  ╚══════╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░╚═════╝░╚══════╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝
+#
+#  Developed by Yakov V. Panov (C) Ling • Black 2020
+#  @site http://ling.black
+from fastapi import APIRouter, Depends
+
+from database import get_db
+from wow.database.models import CharacterModel
+from wow.interface.entity import Character
+
+router = APIRouter()
+
+
+@router.get(
+    "/{name}",
+    response_model=Character
+)
+def get_character(name: str, db=Depends(get_db)):
+    name = name.title()
+    return db.query(CharacterModel).filter(CharacterModel.name == name).first()
---
+++ wow/router/guild.py
@@ -0,0 +1,36 @@
+#  ██╗░░░░░██╗███╗░░██╗░██████╗░░░░██████╗░██╗░░░░░░█████╗░░█████╗░██╗░░██╗
+#  ██║░░░░░██║████╗░██║██╔════╝░░░░██╔══██╗██║░░░░░██╔══██╗██╔══██╗██║░██╔╝
+#  ██║░░░░░██║██╔██╗██║██║░░██╗░░░░██████╦╝██║░░░░░███████║██║░░╚═╝█████═╝░
+#  ██║░░░░░██║██║╚████║██║░░╚██╗░░░██╔══██╗██║░░░░░██╔══██║██║░░██╗██╔═██╗░
+#  ███████╗██║██║░╚███║╚██████╔╝░░░██████╦╝███████╗██║░░██║╚█████╔╝██║░╚██╗
+#  ╚══════╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░╚═════╝░╚══════╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝
+#
+#  Developed by Yakov V. Panov (C) Ling • Black 2020
+#  @site http://ling.black
+from typing import List
+
+from fastapi import APIRouter, Depends
+
+from database import get_db
+from database.client import DataStore
+from database.schemas import DataStore as DataStoreType
+
+router = APIRouter()
+
+
+@router.get(
+    "/",
+    summary="Returns the guild info",
+    response_model=List[DataStoreType]
+)
+def guild_get_info(db=Depends(get_db)):
+    return DataStore.list(db)
+
+
+@router.get(
+    "/{param}",
+    summary="Returns the guild variable info",
+    response_model=DataStoreType
+)
+def guild_get_info(param: str, db=Depends(get_db)):
+    return DataStore.get(db, field=param)
