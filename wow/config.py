b790214391a05d2997e6108e7f63246102cd895a
diego <masnimpu@gmail.com>
2020-10-29 16:43:42 +0300

    Added default paths

---
+++ wow/config.py
@@ -0,0 +1,29 @@
+#  ██╗░░░░░██╗███╗░░██╗░██████╗░░░░██████╗░██╗░░░░░░█████╗░░█████╗░██╗░░██╗
+#  ██║░░░░░██║████╗░██║██╔════╝░░░░██╔══██╗██║░░░░░██╔══██╗██╔══██╗██║░██╔╝
+#  ██║░░░░░██║██╔██╗██║██║░░██╗░░░░██████╦╝██║░░░░░███████║██║░░╚═╝█████═╝░
+#  ██║░░░░░██║██║╚████║██║░░╚██╗░░░██╔══██╗██║░░░░░██╔══██║██║░░██╗██╔═██╗░
+#  ███████╗██║██║░╚███║╚██████╔╝░░░██████╦╝███████╗██║░░██║╚█████╔╝██║░╚██╗
+#  ╚══════╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░╚═════╝░╚══════╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝
+#
+#  Developed by Yakov V. Panov (C) Ling • Black 2020
+#  @site http://ling.black
+
+guild_name = "Престиж"
+server_slug = "gordunni"
+mythic_season = 4
+default_namespace = "profile-eu"
+default_static_namespace = "static-eu"
+
+# Client api
+
+client_id = "b2de0b42d91c4231916cef9ef5a00f95"
+client_secret = "uXXpE3pZ4L2A1uUyIyr6Tv1XY7wG3H3s"
+
+# Blizzard
+
+blizzard_api_url = "https://eu.api.blizzard.com"
+
+# Static data
+default_static_path = 'static'
+default_items_images_path = default_static_path + "/items"
+default_characters_images_path = default_static_path + "/characters"
